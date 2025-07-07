27e9b5b621623c16ba4c31d2e752a87ea104a890
somiya1373 <157354242+somiya1373@users.noreply.github.com>
2025-07-07 15:32:42 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 function sum(a,b){
-    return a+b
+    return a+b;
 }
-function sub(a,b){
-    return a-b
-}
-//module.exports ={
-  //  sum,
-   // sub
-//}
-//  common js module system ---reqire
-module.exports.sum=sum;
-module.exports.sub=sub;
-
-
+module.export=sum
