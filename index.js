efdbf6e6de1cda85718ccb808cd242c9df203df0
somiya1373 <157354242+somiya1373@users.noreply.github.com>
2025-07-16 14:13:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function sum(a,b){
-    return a+b;
+
+let users=[
+    {
+        id:1,
+        name:"Somiya",
+        age:20
+    },
+    {
+        id:2,
+        name:"Keshav",
+        age:17
+    },
+]
+function isAllowed(id){
+    //id ka user dhundo
+    // fir uskaage check kro>18
+
 }
-module.export=sum
