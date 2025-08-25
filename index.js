1dc08a209679f0a9da9087b9f63deb5cda1444bf
somiya1373 <157354242+somiya1373@users.noreply.github.com>
2025-08-25 21:33:35 +0530

    lec21-22

--- index.js
+++ index.js
@@ -1,53 +1,114 @@
-// function getUserData(URL) {
-//     fetch(URL)
-//         .then((res) => {
-//             console.log(res);
-//             return res.json(); // return once
-//         })
-//         .then((data) => {
-//             console.log(data); // parsed JSON data
-//         })
-//         .catch((err) => {
-//             console.log("Error:", err);
-//         });
-// }
+const express=require("express");
+const mongoose=require("mongoose");
+const User=require("./model/users");
+const jwt = require("jsonwebtoken")
+const app=express();
 
-// getUserData("https://jsonplaceholder.typicode.com/users");
-
-
-
-let userContainer = document.querySelector(".user-container");
-console.log(userContainer);
-
-function getUsersData(URL){
-    fetch(URL)
-    .then((res)=>{
-        console.log(res)
-        return res.json()      // to convert to json & return promise
-    })
-    .then((data)=>{
-        console.log(data);
-        data.forEach(user=>{
-            displayUsers(user);
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+console.log(User)
+function isLogin(req, res, next){
+    let token=req.headers.authorization
+    console.log(token);
+    if(!token){
+        return res.json({
+            success:false,
+            message:"please login"
         })
+    }
+    let decode=jwt.verify(token,"okkkkk")
+    console.log(decode)
+    if(!decode)({
+        success:false,
+        message:"invaid token"
     })
-    .catch((err)=>{
-        console.log(err);
-    })
+}next()
 }
 
-function displayUsers(users){
-    let li = document.createElement("li");
-    li.setAttribute("class", "user-item");
-    li.innerHTML = `<div class="userInfo">
-                <h1> ${users.name}</h1>
-                <p>${users.username}</p>
-            </div>
-            <div class="userButton">
-                <button class="user-delete">Delete</button>
-                <button class="user-edit">Edit</button>
-            </div>`
+app.get("/health", (req, res)=>{
+    res.json({
+        status:"Okay",
+        message:"Server is running okay"
+    })
+})
+app.get("/home",isLogin,(req,res)=>{
+    let username;
+    res.json({
+        success:true,
+        message:"welcome"+username
+    })
+})
+app.post("/api/users/signUp",async(req, res)=>{
+   try{
+     let{name, email, password}=req.body;
+    let userExist= await User.findOne({email:email});
+    if(userExist){
+        return res.json({
+            success:false,
+            message:"User already exist"
+        })
+    }
+    let newUser=new User({
+        name:name,
+        email:email,
+        password:password,
+    });
 
-            userContainer.appendChild(li);
-}
-getUsersData("https://jsonplaceholder.typicode.com/users");
+    await newUser.save();
+   return res.json({
+    success:true,
+    message:"User registered successfully, please login to continue "
+   }) 
+   }catch (error){
+    console.log(error.message);
+    res.json({
+        error:{
+            message:error.message
+        }
+    })
+   }
+})
+
+
+app.post("/api/auth/login", async(req, res)=>{
+    try{
+        let{email,password}=req.body;
+    let userExist=await User.findOne({email:email});
+    if(!userExist){
+        return res.json({
+            success:false,
+            message:"You are not registered. Please signup"
+        })
+    }
+    if(password!==userExist.password){
+        return res.json({
+            success:false,
+            message:"Invalid password"
+        })
+    }
+    let token = jwt.sign({"user":userExist},"okkkkk");
+    return res.json({
+        sucess:true,
+        message:"Login successfully",
+        token:token
+    })
+    }catch (error){
+        console.log(error.message)
+        res.json({
+            message:error.message
+        })
+    }
+})
+
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/g26_db')
+  .then(() => console.log('Connected!'))
+  .catch((err)=>{
+    console.log(err);
+  
+  })
+app.listen(3000, ()=>{
+    console.log("Server started");
+})
